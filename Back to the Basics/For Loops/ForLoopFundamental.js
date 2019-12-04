3741025b5bf71d74b7c0f6e23dfc9c19bad13230
askoenig <askoenig94@gmail.com>
2019-12-04 15:00:53 -0600

    countDown challenge

---
+++ Back to the Basics/For Loops/ForLoopFundamental.js
@@ -0,0 +1,7 @@
+// Using a for loop, decrement countDown by one each time the loop runs until it equals 0, making use of looping functionality instead of logging each number separately.
+
+let countDown = 10;
+// ADD CODE HERE
+for (let i = 1; i <= countDown; countDown--) {}
+// Uncomment the below line to check your work
+console.log(countDown); // -> should print 0;
