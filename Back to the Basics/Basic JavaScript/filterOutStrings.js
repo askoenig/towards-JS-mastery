0597757033611e6964cb18bce636721758b066ef
askoenig <askoenig94@gmail.com>
2019-12-28 13:09:00 -0600

    filter out strings from array

---
+++ Back to the Basics/Basic JavaScript/filterOutStrings.js
@@ -0,0 +1,9 @@
+//Create a function that takes an array of non-negative numbers and strings and return a new array without the strings.
+
+function filterArray(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === "number") newArr.push(arr[i]);
+  }
+  return newArr;
+}
