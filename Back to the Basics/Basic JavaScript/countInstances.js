6e3df3efd03b9e8f9051cb69c713aefbb52a26a3
askoenig <askoenig94@gmail.com>
2020-01-01 23:38:35 -0600

    count instances challenge

---
+++ Back to the Basics/Basic JavaScript/countInstances.js
@@ -0,0 +1,11 @@
+// Create a function that takes two strings as arguments and returns the number of times the first string (the single character) is found in the second string
+
+function charCount(myChar, str) {
+  let counter = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === myChar) {
+      counter++;
+    }
+  }
+  return counter;
+}
