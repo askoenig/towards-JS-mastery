5a28aa77be8371158ccbc2d3e0328ddb7568d657
askoenig <askoenig94@gmail.com>
2020-01-16 17:34:56 -0500

    value vs reference

---
+++ Back to the Basics/For Loops/valueVsReference.js
@@ -0,0 +1,14 @@
+// Create a function that returns true if two arrays contain identical values, and false otherwise.
+
+function checkEquals(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
