be329ed40284b3b93bfe1340656cea217ec83ba1
askoenig <askoenig94@gmail.com>
2020-01-29 22:56:19 -0500

    my first solution

--- Back to the Basics/Basic JavaScript/largestSwap.js
+++ Back to the Basics/Basic JavaScript/largestSwap.js
@@ -1,1 +1,11 @@
 // Write a function that takes a two-digit number and determines if it's the largest of two possible digit swaps
+
+function largestSwap(num) {
+  let reversedNum = Number(
+    Array.from(num.toString())
+      .reverse()
+      .join("")
+  );
+  console.log(num, reversedNum);
+  return reversedNum === num || reversedNum < num ? true : false;
+}
