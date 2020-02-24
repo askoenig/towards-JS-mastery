91ea08fc389825a26b733b5ac2aa419aae48a9b1
askoenig <askoenig94@gmail.com>
2020-02-23 22:13:30 -0500

    second attempt

--- Back to the Basics/Basic JavaScript/countVowels.js
+++ Back to the Basics/Basic JavaScript/countVowels.js
@@ -1,6 +1,6 @@
 function countVowels(str) {
   let counter = 0;
-  const vowels = [a, e, i, o, u];
+  const vowels = ["a", "e", "i", "o", "u"];
   for (let i = 0; i < str.length; i++) {
     if (vowels.includes(str[i])) {
       counter++;
