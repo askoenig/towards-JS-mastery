3606036fdf5865ce678c3635ca97dc9dcef323f8
askoenig <askoenig94@gmail.com>
2020-02-23 22:13:04 -0500

    first attempt

---
+++ Back to the Basics/Basic JavaScript/countVowels.js
@@ -0,0 +1,10 @@
+function countVowels(str) {
+  let counter = 0;
+  const vowels = [a, e, i, o, u];
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i])) {
+      counter++;
+    }
+  }
+  return counter;
+}
