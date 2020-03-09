6bd6007f6a5ec6b5dac8e469ede7c61e3b2034b0
askoenig <askoenig94@gmail.com>
2020-03-08 22:21:19 -0400

    second pass refactor attempt

--- Control Flow/leapYear.js
+++ Control Flow/leapYear.js
@@ -17,3 +17,13 @@ const isLeap = year => {
     return false;
   }
 };
+
+//refactor attempt:
+
+const isLeap = () => {
+  if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
+    return true;
+  } else {
+    return false;
+  }
+};
