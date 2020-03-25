4cb1d55297b923db10d8864e8566a24ed0cf3c97
askoenig <askoenig94@gmail.com>
2020-03-25 15:57:31 -0500

    added 2nd pass and sumaaries

--- Algorithms /Easy Algos/collatz_conjecture.js
+++ Algorithms /Easy Algos/collatz_conjecture.js
@@ -24,4 +24,36 @@ export const steps = (n, count = 0) => {
   }
 };
 
-//
+// above solution failed because high value args blow out the call stack
+// SECOND PASS:
+
+export const steps = (n, count = 0) => {
+  let counter = count;
+  console.log(n, counter);
+  if (n <= 0) {
+    throw "Only positive numbers are allowed";
+  } else if (n > 10000) {
+    for (let i = 0; i < n; i++) {
+      if (n % 2 === 0) {
+        n = n / 2;
+        counter++;
+      } else if (n % 3 === 0 || n % 5 === 0) {
+        n = n * 3 + 1;
+        counter++;
+      }
+    }
+    return counter;
+  } else if (n % 2 === 0) {
+    let x = n / 2;
+    counter++;
+    return steps(x, counter);
+  } else if (n % 3 === 0 || n % 5 === 0) {
+    let x = n * 3 + 1;
+    counter++;
+    return steps(x, counter);
+  } else {
+    return counter;
+  }
+};
+
+// here I tried to catch high value args to run them through a loop so as to avoid blowing out the stack via recursion. I think this acutally might have worked if my for loop syntax was better. Regardless, still far from optimal.
