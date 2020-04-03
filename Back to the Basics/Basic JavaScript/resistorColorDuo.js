df804faf54bb4f7d9c860e6fa84b332a74e26258
askoenig <askoenig94@gmail.com>
2020-04-03 00:14:33 -0500

    first pass

--- Back to the Basics/Basic JavaScript/resistorColorDuo.js
+++ Back to the Basics/Basic JavaScript/resistorColorDuo.js
@@ -1,3 +1,6 @@
 // The program will take color names as input and output a two digit number, even if the input is more than two colors!
 
 // The colors are mapped to the numbers from 0 to 9 in the sequence: Black - Brown - Red - Orange - Yellow - Green - Blue - Violet - Grey - White
+
+export const decodedValue = ([firstColor, secondColor]) =>
+  Number("" + colorValues[firstColor] + colorValues[secondColor]);
