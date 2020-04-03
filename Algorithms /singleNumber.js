802d8d71d9e505e7542fe14f21a39f099e978bb7
askoenig <askoenig94@gmail.com>
2020-04-03 00:10:49 -0500

    first pass

--- Algorithms /singleNumber.js
+++ Algorithms /singleNumber.js
@@ -1,1 +1,13 @@
 // Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+var singleNumber = function(nums) {
+  while (nums.length > 0) {
+    let num = nums.shift();
+    if (nums.includes(num)) {
+      let index = nums.indexOf(num);
+      nums.splice(index, 1);
+    } else {
+      return num;
+    }
+  }
+};
